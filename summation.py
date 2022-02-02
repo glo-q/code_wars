f98f35117b742f81b7aff33d624729bc5d51b56d
Rafael <89470801+glo-q@users.noreply.github.com>
2022-02-02 16:24:28 +0100

    Create summation.py

---
+++ summation.py
@@ -0,0 +1,22 @@
+# Summation Write a program that finds the summation of every number from 1 to num. The number will always be a
+# positive integer greater than 0.
+#
+# For example:
+#
+# summation(2) -> 3
+# 1 + 2
+#
+# summation(8) -> 36
+# 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+def summation(num):
+    sum = 0
+    for n in range(num + 1):
+        sum += n
+
+    return sum
+
+
+# print(Wooooooo...)
+# def summation(num):
+#     return sum(xrange(num + 1))
