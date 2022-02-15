21ba5796044f0173228d6146e4ea797657531510
Rafael <89470801+glo-q@users.noreply.github.com>
2022-02-15 23:23:50 +0100

    Update categorize_new_member.py

--- categorize_new_member.py
+++ categorize_new_member.py
@@ -1,15 +1,33 @@
-# The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with
-# an application form that will tell prospective members which category they will be placed.
-#
-# To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club,
-# handicaps range from -2 to +26; the better the player the lower the handicap.
-#
-# Input will consist of a list of pairs. Each pair contains information for a single potential member.
-# Information consists of an integer for the person's age and an integer for the person's handicap.
-#
-# Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective
-# member is to be placed in the senior or open category.
+# CodeWars Python Solutions
 
+---
+
+## Categorize New Member
+
+
+
+### Description:
+
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with
+an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club,
+handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input will consist of a list of pairs. Each pair contains information for a single potential member.
+Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective
+member is to be placed in the senior or open category.
+
+
+---
+
+
+### Solution
+
+
+```python
 def open_or_senior(data):
     category = []
     for x in data:
@@ -18,8 +36,11 @@ def open_or_senior(data):
         else:
             category += ("Open",)
     return category
+```
+
+---
+### Comment
 
 
-# Best found:
-# def openOrSenior(data):
-#   return ["Senior" if age >= 55 and handicap >= 8 else "Open" for (age, handicap) in data]
+
+[See on CodeWars.com](https://www.codewars.com/users/ITRonin)
